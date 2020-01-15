65eebe221c6ffdc77f5116df14a0f75ffff5f37f
Raina Shin <yewon95@vt.edu>
2020-01-15 12:03:22 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 'use strict';
 
-function getDogImages(amount) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${amount}`)
+function getDogImages(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then(responseJson => {
+      if (responseJson.status === 'success') {
+        displayResults(responseJson);
+      } else {
+        displayError(responseJson);
+      }
+    }
+    )
+    .catch(error => alert('Something went wrong. Try again later.'));  
+}
+
+function displayError(responseJson) {
+  let html = `<h2>Error! ${responseJson.code}</h2> <p>${responseJson.message}</p>`;
+  $('.results').html(html);
+  $('.results').removeClass('hidden');
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   //display the results section
-  let html = '<h2>Look at these dogs!</h2>';
-  responseJson.message.forEach(img => {
-    html += `<img src='${img}' alt='Doggy'/>`;
-  });
+  let html = `<h2>Look at these dogs!</h2> <img src='${responseJson.message}' alt='Doggy'/>`;
   $('.results').html(html);
   $('.results').removeClass('hidden');
 }
